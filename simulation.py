e0234f45ede6147579997055fb81ea21a67378bb
Andrew Chen <andrewxinchen@gmail.com>
2021-11-05 12:19:11 -0700

    acceleration implemented

--- simulation.py
+++ simulation.py
@@ -4,103 +4,139 @@ import matplotlib.animation as ani
 
 ni = 200   # initial number of locusts
 n = 5000
-L = 50
-dt = .01
-nx = 50    # number of pheromone/grass cells along one direction
-ny = 50
-nBig = 10   # number of big cells along one direction
-p0 = .01    # normal locust's pheromone emission
-p1 = .05    # swarming locust's pheromone emission
-pt = .02    # pheromone decay rate
-f0 = .002    # amount a normal locust eats
-f1 = .005    # amount a swarming locust eats
-ft = .005    # food growth rate
-birth0 = .003    # birth rate (if fed)
-birth1 = .010    
-death0 = .2    # death rate (if hungry)
-death1 = .2
+L = 60
+t = 0
+dt = .05
+nx = L   # number of pheromone/grass cells along one direction
+ny = L
+nBig = 25   # number of big cells along one direction
+p0 = 1    # normal locust's pheromone emission
+p1 = 5    # swarming locust's pheromone emission
+pt = 2    # pheromone decay rate
+f0 = .2    # amount a normal locust eats
+f1 = .5    # amount a swarming locust eats
+ft = .2    # food growth rate
+birth0 = .1    # birth rate (if fed)
+birth1 = .5    
+death0 = 1    # death rate (if hungry)
+death1 = 2
+deathLonely = .1
 pActivateThresh = 1
 pDeactivateThresh = .5
 activateProb = .3
+followNeighbors = .1
+followFood = .2
+followPheromones = .02
+maxSpeed0 = 5
+maxSpeed1 = 10
 
-x = np.zeros([n, 2]) - 1000          # position
+x = np.zeros([n, 2]) - 600          # position
 x[:ni] = np.random.rand(ni, 2)*L
 v = np.zeros([n, 2])              # velocity
 v[:ni] = (np.random.rand(ni, 2)-.5)*10
 p = np.zeros([nx, ny])              # pheromones
-f = np.ones([nx, ny])              # food
+f = np.zeros([nx, ny])+.3              # food
 dens = np.zeros([nx, ny])           # number of locusts in each grid square
 state = np.zeros([n])              # whether each locust is normal, swarming, or dead (1, 2, 0)
 state[:ni] = np.ones([ni])
-bigGrid[   
+bigGrid = np.zeros([nBig, nBig])    # grid used for finding locust interactions
+bgPop = np.zeros([nBig, nBig])
+bgVelocity = np.zeros([nBig, nBig, 2])
+
 
 def step():
-    global x, v, p, f, state, dens, distances
+    global t, x, v, p, f, state, dens, distances
     x = (x + v*dt)%L
 
+    temp = max(-.2, np.cos(t/10)+.5) #temperature
+
     xgrid = (((x+.5)*nx/L).astype(int))%L
-
-    use bigGrid
-    fff = 0
-    # find neighbors
-    for i in range(n):
-        for j in range(n):
-            fff += j
-    print(fff)
-
+    xbGrid = (((x+L/nBig)*nBig/L).astype(int))%nBig
     randoms = np.random.rand(n)
     randoms2 = np.random.rand(n)
+    randoms3 = np.random.rand(n, 2)-.5
+    for i in range(n):
+        if state[i] > 0:
+            bgPop[xbGrid[i, 0], xbGrid[i, 1]] += 1
+            bgVelocity[xbGrid[i, 0], xbGrid[i, 1], :] += v[i, :]
+    for i in range(n):
+        if state[i] > 0:
+            xbi = xbGrid[i, 0]
+            ybi = xbGrid[i, 1]
+            v[i] = v[i]*(1-followNeighbors) + bgVelocity[xbi, ybi, :]/bgPop[xbi, ybi]*followNeighbors
+
+            xi = xgrid[i, 0]
+            yi = xgrid[i, 1]
+            v[i, 1] += followFood * (f[xi, (yi+1)%L] - f[xi, (yi-1)%L])
+            v[i, 1] *= (1 + .3*followFood * (f[xi, (yi+1)%L] + f[xi, (yi+1)%L] - 2*f[xi, yi]))
+            v[i, 0] += followFood * (f[(xi+1)%L, yi] - f[(xi-1)%L, yi])
+            v[i, 0] *= (1 + .3*followFood * (f[(xi+1)%L, yi] + f[(xi-1)%L, yi] - 2*f[xi, yi]))
+
+            v[i, 1] += followPheromones * (p[xi, (yi+1)%L] - p[xi, (yi-1)%L])
+            v[i, 0] += followPheromones * (p[(xi+1)%L, yi] - p[(xi-1)%L, yi])
+
+            if state[i] == 1:
+                if v[i, 0]*v[i, 0] + v[i, 1]*v[i, 1] > maxSpeed0*maxSpeed0*(.5+.5*temp):
+                    v[i] = v[i]*.9
+            else:
+                if v[i, 0]*v[i, 0] + v[i, 1]*v[i, 1] > maxSpeed1*maxSpeed1*(.5+.5*temp):
+                    v[i] = v[i]*.9
+
+            v[i] += randoms3[i]
+
 
     for i in range(n): 
-        # xi = (int((x[i, 0]+.5)*nx/L))%L      # locust's grid position
-        # yi = (int((x[i, 1]+.5)*ny/L))%L
         xi = xgrid[i, 0]
         yi = xgrid[i, 1]
 
         if state[i] == 1:           # update pheromones and food
-            if (p[xi, yi] > pActivateThresh) and (randoms2[i] < activateProb):
+            if (p[xi, yi]+temp-.5 > pActivateThresh) and (randoms2[i] < activateProb):
                 state[i] = 2
-            if f[xi, yi] > f0:
-                f[xi, yi] -= f0
-                if randoms[i] < birth0:
+            if f[xi, yi] > f0*dt*(temp+.5):
+                f[xi, yi] -= f0*dt*(temp+.5)
+                if randoms[i] < birth0*dt*(temp+.5):
                     birth(x[i], v[i])
-                p[xi, yi] += p0
-            elif randoms[i] < death0:
+                p[xi, yi] += p0*dt*(temp+.5)
+            elif randoms[i] < death0*dt:
                 state[i] = 0  
-                x[i] = [-1000,-1000]
+                x[i] = [-600,-600]
                 v[i] = [0,0]
+                f[xi, yi] += .1
+            if p[xi, yi] < .1 and randoms[i] < deathLonely*dt:
+                state[i] = 0  
+                x[i] = [-600,-600]
+                v[i] = [0,0]
+                f[xi, yi] += .1
 
         if state[i] == 2:
-            if (p[xi, yi] < pDeactivateThresh):
+            if (p[xi, yi]+temp-.5 < pDeactivateThresh):
                 state[i] = 1
-            if f[xi, yi] > f1:
-                f[xi, yi] -= f1
-                if randoms[i] < birth1:
+            if f[xi, yi] > f1*dt*(temp+.5):
+                f[xi, yi] -= f1*dt*(temp+.5)
+                if randoms[i] < birth1*dt*(temp+.5):
                     birth(x[i], v[i])
-                p[xi, yi] += p1
-            elif randoms[i] < death1:
+                p[xi, yi] += p1*dt*(temp+.5)
+            elif randoms[i] < death1*dt:
                 state[i] = 0
-                x[i] = [-1000,-1000]
+                x[i] = [-600,-600]
                 v[i] = [0,0]
+                f[xi, yi] += .1
 
 
-    # update velocities
-    # go towards food + pheromones
-    # go with neighbors
-
-    p *= (1-pt)     # decay pheromones
-    p = .96*f + .01*(np.roll(p, 1, axis=0) + 
-                np.roll(p, 1, axis=1) +      # diffuse food
+    p *= (1-pt*dt)     # decay pheromones
+    p = (1-.4*dt)*p + .1*dt*(np.roll(p, 1, axis=0) + 
+                np.roll(p, 1, axis=1) +      # diffuse pheromones
                 np.roll(p, -1, axis=0) + 
                 np.roll(p, -1, axis=-1))   
 
-    f = f*(1 + (1-f)*ft + .001)    # grow food (growth rate peaks at ft, when f=.5)
-    f = .96*f + .01*(np.roll(f, 1, axis=0) + 
+    f = f*(1 + (1-f)*ft*dt*temp + .001*dt*temp)    # grow food (growth rate peaks at ft, when f=.5)
+    f = (1-.4*dt)*f + .1*dt*(np.roll(f, 1, axis=0) + 
                     np.roll(f, 1, axis=1) +      # diffuse food
                     np.roll(f, -1, axis=0) + 
                     np.roll(f, -1, axis=-1))   
+    t += dt
 
-    print(np.bincount(state.astype(int)))
+    #print(np.bincount(state.astype(int)))
 
 def birth(pos, vel):
     global x, v, state
@@ -118,26 +154,36 @@ def birth(pos, vel):
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plt.xlim(-.5, L-.5)
-plt.ylim(-.5, L-.5)
+ax2 = fig.add_subplot(122)
+ax.set_xlim(.5, L-.5)
+ax.set_ylim(.5, L-.5)
+ax.axis('off')
 ax.set_aspect('equal', adjustable='box')
+ax2.set_xlim(.5, L-.5)
+ax2.set_ylim(.5, L-.5)
+ax2.axis('off')
+ax2.set_aspect('equal', adjustable='box')
 
-particles = ax.scatter([], [], c=[], s=2, vmin=.5, vmax=2.5, cmap='YlOrBr', alpha=.5)
+particles = ax.scatter([], [], c='#ff4d40', s=2, vmin=.5, vmax=2.5, cmap='YlOrBr', alpha=.5)
 background = ax.imshow(np.zeros([nx,nx]), cmap='Greens', vmin=0, vmax=1.5)
+background2 = ax.imshow(np.zeros([nx,nx]), cmap='Reds', vmin=0, vmax=1.5, alpha=.2)
 
 def init():
-    global x, v, p
-    background.set_array(p)
+    global x, v, p, t
+    background.set_array(f)
+    background2.set_array(p)
     particles.set_offsets(x)
-    return background, particles
+    return background, background2, particles
 
 def animate(i):
-    global x, v, p
+    global x, v, p, t
     step()
-    particles.set_offsets(x[:, [1, 0]])
-    particles.set_array(state)
     background.set_array(f)
-    return background, particles
+    background2.set_array(p)
+    particles.set_offsets(x[:, [1, 0]])
+    #particles.set_array(state)
+
+    return background, background2, particles
 
 animation = ani.FuncAnimation(fig, animate, frames=600,
                               interval=20, blit=True, init_func=init)
